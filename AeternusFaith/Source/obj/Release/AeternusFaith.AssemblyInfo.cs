b9f1d226468fbe692ccc9ecd04f732c76036a862
Tony Andraza <tandraza1@gmail.com>
2026-05-07 11:01:55 -0400

    Spell requirements enhancements

--- AeternusFaith/Source/obj/Release/AeternusFaith.AssemblyInfo.cs
+++ AeternusFaith/Source/obj/Release/AeternusFaith.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeternusFaith")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a821351a7d131a04a1b55bf07ed934739da4d16b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38ad9c4d76b619f911e444c8444fe1068309118")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeternusFaith")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeternusFaith")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
