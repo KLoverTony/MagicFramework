3d6e7041b06faddc45fbfdcb9c96f10054661f2f
Tony Andraza <tandraza1@gmail.com>
2026-05-30 22:06:17 -0400

    Added medicinal herb to ink production.

--- AeternusFaith/Source/obj/Debug/AeternusFaith.AssemblyInfo.cs
+++ AeternusFaith/Source/obj/Debug/AeternusFaith.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeternusFaith")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920113f2bca58f27442dce1e8606d2c019671f30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac3c751e65cf221f8af5d745c762bceda7602de")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeternusFaith")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeternusFaith")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
